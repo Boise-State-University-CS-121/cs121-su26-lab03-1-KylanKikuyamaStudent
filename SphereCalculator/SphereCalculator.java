f18eccacb2bbb69c43b14629cff5bf4dfddb014c
Kylan Kikuyama <kylankikuyama@u.boisestate.edu>
2026-06-16 23:04:57 -0600

    Lab Activity 1 is completed.

---
+++ SphereCalculator/SphereCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class SphereCalculator 
+{
+    public static void main(String[] args)
+    {
+        Scanner kbd = new Scanner(System.in);
+        Random myGen = new Random();
+
+        //Input Processing
+        System.out.print("Enter the sphere's radius: ");
+        double radius = kbd.nextDouble();
+
+        //Math Processing
+        double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3.0);
+        double surfaceArea = 4.0 * Math.PI * Math.pow(radius, 2.0);
+
+        //Output Processing
+        System.out.printf("Volume: %.4f\n", volume);
+        System.out.printf("Surface area: %.4f\n" , surfaceArea);
+
+
+    }   
+}
